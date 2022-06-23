2f938ab90329f31d447d451daea3e7b60d2c3854
Szwajowy <krzycza96@gmail.com>
2022-06-23 12:46:35 +0200

    Create ProductsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ProductsComponent } from "./products/pages/products/products.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ProductsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
