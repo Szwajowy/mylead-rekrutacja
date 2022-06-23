c284d516a8204b0ad91b0c1264fd729e68280631
Szwajowy <krzycza96@gmail.com>
2022-06-23 12:47:26 +0200

    Create AdminComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AdminComponent } from "./admin/pages/admin/admin.component";
 import { ProductsComponent } from "./products/pages/products/products.component";
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent, AdminComponent, ProductsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
