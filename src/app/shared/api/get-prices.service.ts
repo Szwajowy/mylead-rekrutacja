f014d97ede50c7ef41d555bdd2a7d07a7763a997
Szwajowy <krzycza96@gmail.com>
2022-06-24 13:00:46 +0200

    Create service for getting prices

---
+++ src/app/shared/api/get-prices.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { API_URL, API_ENDPOINTS } from "../constants/API_ENDPOINTS";
+import { Price } from "../models/Price";
+
+@Injectable({
+  providedIn: "root",
+})
+export class GetPricesService {
+  constructor(private http: HttpClient) {}
+
+  getAllPrices(): Observable<Price[]> {
+    return this.http.get<Price[]>(`${API_URL} + ${API_ENDPOINTS.prices}`);
+  }
+
+  getPricesForProduct(productId: number): Observable<Price[]> {
+    return this.http.get<Price[]>(
+      `${API_URL} + ${API_ENDPOINTS.prices}?productId=${productId}`
+    );
+  }
+}
---
+++ src/app/shared/models/Price.ts
@@ -0,0 +1,6 @@
+export interface Price {
+  id: number;
+  value: number;
+  productId: number;
+  createdAt: Date;
+}
